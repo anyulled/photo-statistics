a8a5bcd5cd860124cad863e14bb75dd8bd5ffb6e
Anyul Rivas <anyul.rivas@ingrammicro.com>
2025-04-22 10:13:43 +0200

    ci: test coverage for sonar

--- generate-coverage.sh
+++ generate-coverage.sh
@@ -17,7 +17,7 @@ grcov target/coverage \
         -o target/coverage \
         --ignore-not-existing \
         --keep-only 'src/*' \
-        --output-types html,cobertura
+        --output-types html,cobertura,lcov
 xmllint --xpath "concat('Coverage: ', 100 * string(//coverage/@line-rate), '%')" target/coverage/cobertura.xml
 cat target/coverage/cobertura.xml
 sonar-scanner --debug > sonar.log
